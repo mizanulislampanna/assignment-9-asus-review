953b0c7a7b16c3a88c795e5c3f28e0528fc211af
Mizanul Islam Panna <mizanulislampanna>
2022-04-04 23:20:30 +0600

    header added

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
+const Header = () => {
+  return (
+    <div>
+      <nav className="navbar">
+        <Link className="routs" to="/home">
+          HOME
+        </Link>
+        <Link className="routs" to="/reviews">
+          REVIEWS
+        </Link>
+        <Link className="routs" to="/dashboard">
+          DASHBOARD
+        </Link>
+        <Link className="routs" to="/blogs">
+          BLOGS
+        </Link>
+        <Link className="routs" to="/about">
+          ABOUT
+        </Link>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
