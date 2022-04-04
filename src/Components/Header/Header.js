bde7d2faaad4a2d58a09c1c3404688d2c1e73d2a
Mizanul Islam Panna <mizanulislampanna>
2022-04-05 01:08:56 +0600

    home page added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import "./Header.css";
 const Header = () => {
   return (
-    <div>
+    <div className="nav-container">
       <nav className="navbar">
         <Link className="routs" to="/home">
           HOME
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,27 @@
 import React from "react";
+import "./Home.css";
 
 const Home = () => {
   return (
-    <div>
-      <h2>This is Home sweet home</h2>
+    <div className="home-continer">
+      <div className="home-text">
+        <h2>ASUS ZenBook Flip</h2>
+        <h2>Ultra Slim 2-in-1</h2>
+        <p>
+          Intel Evo Platform - The perfect combination of performance,
+          responsiveness and battery life. Enjoy amazing performance with the
+          11th Gen Intel Core i7 processor with Intel Iris Xe graphics.
+        </p>
+        <button className="live-demo-btn">
+          <p>Live Demo</p>
+        </button>
+      </div>
+      <div className="image-container">
+        <img
+          src="https://m.media-amazon.com/images/I/81kaVz6wxXS._AC_SX679_.jpg"
+          alt=""
+        />
+      </div>
     </div>
   );
 };
