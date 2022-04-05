d50ecc90afb90e35e7c8104ca2f8fc2a2d4a6756
Mizanul Islam Panna <mizanulislampanna>
2022-04-05 23:56:24 +0600

    all done

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -2,8 +2,16 @@ import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>Do you want to Know About me . send me some money</h2>
+    <div style={{ textAlign: "center" }}>
+      <h2>Do you want to Know About me?</h2>
+      <p>
+        hello, my name is Mizanul islam panna. i am a web developer & WordPress
+        Expert. I have been working last 2 years on WordPress development and
+        web designing sector and worked various marketplaces, locally and
+        internationally. I am always available on Fiverr to communicate with
+        you. If you need any help just knock me through message i will catch you
+        instant. Thank you
+      </p>
     </div>
   );
 };
